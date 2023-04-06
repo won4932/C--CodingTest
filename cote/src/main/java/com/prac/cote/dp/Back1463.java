30fd81f29b95671db6cf9dfdd35ae72dd777011c
WontaeSeong <won4932@gmail.com>
2023-04-06 09:57:07 +0900

    back 1463 transfer

---
+++ cote/src/main/java/com/prac/cote/dp/Back1463.java
@@ -0,0 +1,84 @@
+package com.prac.cote.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back1463 {
+
+    static ArrayList<Integer>[] seq;
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(Back1463.class, args);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(reader.readLine());
+
+        for(int i = 0; i < t; i++) {
+            StringTokenizer st;
+            st = new StringTokenizer(reader.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(reader.readLine());
+
+            long[] building = new long[n+1];
+            seq = new ArrayList[n+1];
+            int[] ck = new int[n+1];
+
+            for(int a = 1; a < n + 1; a++) {
+                building[a] = Long.parseLong(st.nextToken());
+                seq[a] = new ArrayList<>();
+            }
+
+            for(int a = 0; a < k; a++) {
+                StringTokenizer st2 = new StringTokenizer(reader.readLine());
+
+                int start = Integer.parseInt(st2.nextToken());
+                int end = Integer.parseInt(st2.nextToken());
+
+                seq[start].add(end);
+
+                ck[end]++;
+            }
+
+            int w = Integer.parseInt(reader.readLine());
+
+            long[] time = new long[n+1];
+
+            bfs(time, building, ck);
+
+            System.out.println(time[w]);
+        }
+    }
+
+    public static void bfs(long[] time, long[] building, int[] ck) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 1; i < ck.length; i++) {
+            if(ck[i] == 0) {
+                time[i] = building[i];
+                queue.add(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for(int i = 0; i < seq[current].size(); i++) {
+                int next = seq[current].get(i);
+                time[next] = Math.max(time[current] + building[next], time[next]);
+                ck[next]--;
+                if(ck[next] == 0) queue.add(next);
+            }
+        }
+    }
+}
