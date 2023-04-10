70fc56180ea30df98a05eb02a0d34484d1e8b638
WontaeSeong <won4932@gmail.com>
2023-04-10 09:57:07 +0900

    back 1654 transfer

--- cote/src/main/java/com/prac/cote/bs/Back1654.java
+++ cote/src/main/java/com/prac/cote/bs/Back1654.java
@@ -3,6 +3,7 @@ package com.prac.cote.bs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 import org.springframework.boot.SpringApplication;
@@ -10,31 +11,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Back1654 {
-
+    static int get[];
     public static void main(String[] args) throws IOException {
-        SpringApplication.run(Back1654.class, args);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int T = Integer.parseInt(br.readLine());
-
-        int[] zero = new int[41];
-        int[] one = new int[41];
-
-        zero[0] = 1;
-        zero[1] = 0;
-        one[0] = 0;
-        one[1] = 1;
-
-        for(int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-            for(int j = 2; j <= N; j++) {
-                zero[j] = zero[j-1] + zero[j-2];
-                one[j] = one[j-1] + one[j-2];
-            }
-            System.out.println(zero[N] + " " + one[N]);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        get = new int[N];
+        int sum = 0;
+        for(int i=0; i<N; i++) {
+            get[i] = Integer.parseInt(br.readLine());
+            sum += get[i];
         }
-
+        Arrays.sort(get);
+        long left = 1;
+        long right = get[N-1];
+        while(left<=right) {
+            long mid = (left+right)/2;
+            int ans = division(mid);
+            if(K>ans) {
+                right = mid -1;
+            }
+            else if(K<=ans){
+                left = mid + 1;
+            }
+        }
+        System.out.println(right);
     }
-
+    private static int division(long n) {
+        int ans = 0;
+        for(int i : get) {
+            ans+= i/n;
+        }
+        return ans;
+    }
 }
