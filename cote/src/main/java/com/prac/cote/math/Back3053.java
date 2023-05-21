c71ea23a683ddf8517ca080e614708a750712ffe
WontaeSeong <won4932@gmail.com>
2023-05-21 09:57:07 +0900

    back 3053 transfer

---
+++ cote/src/main/java/com/prac/cote/math/Back3053.java
@@ -0,0 +1,24 @@
+package com.prac.cote.math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back3053 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		double R = Integer.parseInt(br.readLine());
+		bw.write(String.format("%.6f", Math.PI*Math.pow(R, 2))+ "\n");
+		bw.write(String.format("%.6f", 2*Math.pow(R, 2)) + "\n");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
