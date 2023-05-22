0d1fb1029ca3ae40d7a4ab79107c7596d058d431
WontaeSeong <won4932@gmail.com>
2023-05-22 09:57:07 +0900

    back 10250 transfer

---
+++ cote/src/main/java/com/prac/cote/math/Back10250.java
@@ -0,0 +1,30 @@
+package com.prac.cote.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back10250 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int H = Integer.parseInt(st.nextToken());
+			int W = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int floor = N%H, room = N/H+1;
+			if(N%H==0) {
+				room=N/H;
+				floor=H;
+			}
+			System.out.println(floor*100+room);
+		}
+
+	}
+}
