2ff00f077bdef253b2a2acf3b7134bce19823fb4
WontaeSeong <won4932@gmail.com>
2023-08-19 00:53:40 +0900

    back 1029 ctime error

--- cote/src/main/java/com/prac/cote/dp/Back1029.java
+++ cote/src/main/java/com/prac/cote/dp/Back1029.java
@@ -7,10 +7,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class Back1029 {
+
+    static int result = 0;
+
     public static void main(String[] args) throws IOException {
         SpringApplication.run(Back1029.class, args);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -19,19 +22,50 @@ public class Back1029 {
 
         boolean[] visited = new boolean[t];
 
-        String[] buy = new String[t];
+        String[] rawBuy = new String[t];
+
+        int[][] buy = new int[t][t];
 
         for(int a = 0; a < t; a++) {
-            buy[a] = reader.readLine();
+            rawBuy[a] = reader.readLine();
         }
 
-        for(int b = 0; b < buy.length; b++) {
-            visited[b] = true;
+        String[] rawBuy2 = Arrays.copyOfRange(rawBuy, 1, t);
 
+        Arrays.sort(rawBuy2);
+
+        buy[0] = Arrays.stream(rawBuy[0].split(""))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+
+        String[] test2 = rawBuy[0].split("");
+
+        for(int i = 1; i < t; i++) {
+            String[] temp = rawBuy2[i-1].split("");
+            for(int j = 0; j < t; j++) {
+                buy[i][j] = Integer.parseInt(temp[j]);
+            }
         }
+
+        dp(0, visited, buy, 1, 1);
+
+        System.out.println(result);
     }
 
-    public void dp() {
+    public static void dp(int celler, boolean[] visited, int[][] buy, int depth, int price) {
+        visited[celler] = true;
 
+        if(depth == buy.length+1 || price == 9) {
+            return;
+        }
+
+        for(int c = celler; c < buy.length; c++) {
+            if(visited[c] || buy[celler][c] == 0 || buy[celler][c] < price) continue;
+
+            result = Math.max(result, depth+1);
+            dp(c, visited, buy, depth+1, buy[celler][c]);
+        }
+
+        visited[celler] = false;
     }
 }
