bb5086fca338b3159e69e8a660ddcfe2390626d8
WontaeSeong <won4932@gmail.com>
2023-08-21 00:13:57 +0900

    back 1029 memoryzation

--- cote/src/main/java/com/prac/cote/dp/Back1029.java
+++ cote/src/main/java/com/prac/cote/dp/Back1029.java
@@ -6,66 +6,53 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class Back1029 {
-
-    static int result = 0;
+    static int[][][] dpList;
 
     public static void main(String[] args) throws IOException {
+
         SpringApplication.run(Back1029.class, args);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int t = Integer.parseInt(reader.readLine());
 
-        boolean[] visited = new boolean[t];
-
-        String[] rawBuy = new String[t];
-
         int[][] buy = new int[t][t];
+        dpList = new int[t][1 << t][10];
 
-        for(int a = 0; a < t; a++) {
-            rawBuy[a] = reader.readLine();
-        }
-
-        String[] rawBuy2 = Arrays.copyOfRange(rawBuy, 1, t);
-
-        Arrays.sort(rawBuy2);
-
-        buy[0] = Arrays.stream(rawBuy[0].split(""))
-            .mapToInt(Integer::parseInt)
-            .toArray();
-
-        String[] test2 = rawBuy[0].split("");
-
-        for(int i = 1; i < t; i++) {
-            String[] temp = rawBuy2[i-1].split("");
-            for(int j = 0; j < t; j++) {
-                buy[i][j] = Integer.parseInt(temp[j]);
+        for (int a = 0; a < t; a++) {
+            String str = reader.readLine();
+            for (int b = 0; b < t; b++) {
+                if(str.charAt(b) != '0') buy[a][b] = str.charAt(b) - '0';
             }
         }
 
-        dp(0, visited, buy, 1, 1);
-
-        System.out.println(result);
+        System.out.println(dp(0, 1 << (t -1), buy, 1, 0));
     }
 
-    public static void dp(int celler, boolean[] visited, int[][] buy, int depth, int price) {
-        visited[celler] = true;
+    public static int dp(int celler, int mask, int[][] buy, int sum, int price) {
+        int untilNowSum = sum;
 
-        if(depth == buy.length+1 || price == 9) {
-            return;
+        int compSum;
+
+        if (dpList[celler][mask][price] != 0)
+        {
+            return dpList[celler][mask][price];
         }
 
-        for(int c = celler; c < buy.length; c++) {
-            if(visited[c] || buy[celler][c] == 0 || buy[celler][c] < price) continue;
+        for (int c = 0; c < buy.length; c++) {
+//            if ((mask & (1 << c -1)) != 0 || buy[celler][c] == 0 || buy[celler][c] < price) continue;
+            if((mask | (1 << c -1)) != mask & buy[celler][c] >= price) {
+                compSum = dp(c, mask | (1 << (c - 1)), buy, sum + 1, buy[celler][c]);
+                if(untilNowSum < compSum) {
+                    untilNowSum = compSum;
+                }
+            }
 
-            result = Math.max(result, depth+1);
-            dp(c, visited, buy, depth+1, buy[celler][c]);
         }
 
-        visited[celler] = false;
+        dpList[celler][mask][price] = untilNowSum;
+        return untilNowSum;
     }
 }
