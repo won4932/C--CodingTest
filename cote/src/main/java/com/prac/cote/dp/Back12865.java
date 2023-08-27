d478cf655899b5d74404d26e18c38179fc6eb3c0
WontaeSeong <won4932@gmail.com>
2023-08-27 23:06:43 +0900

    back 12865 clear

--- cote/src/main/java/com/prac/cote/dp/Back12865.java
+++ cote/src/main/java/com/prac/cote/dp/Back12865.java
@@ -11,56 +11,46 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Back12865 {
 
+    static Integer[][] dp;
+
     public static void main(String[] args) throws IOException {
 
         SpringApplication.run(Back12865.class, args);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        st = new StringTokenizer(reader.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int result = 0;
+        int[] W = new int[N + 1]; // 무게
+        int[] V = new int[N + 1]; // 가치
+        dp = new Integer[N + 1][K + 1];
 
-        int[][] dp = new int[N][N];
-
-        int[] W = new int[N];
-        int[] V = new int[N];
-
-        for(int a = 0; a < N; a++) {
-            st = new StringTokenizer(reader.readLine());
-            W[a] = Integer.parseInt(st.nextToken());
-            V[a] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int b = 0; b < N -1 ; b++) {
-            dp[b][0] = W[b];
-            dp[b][1] = V[b];
-
-            for(int c = b + 1; c < N; c++) {
-                if(W[b]+W[c] > K) continue;
-
-                dfs(c + 1, V[b] + V[c], N);
-
-
-            //     if(dp[b][0] + W[c] > K) continue;
-            //     for(int d = c + 1; d < N; d++) {
-            //
-            //     }
-            //     dp[b][0] += W[c];
-            //     dp[b][1] = Math.max(dp[b][1], dp[b][1] + V[c]);
-            }
-            // result = Math.max(result, dp[b][1]);
-        }
-
-        System.out.println(result);
+        System.out.println(dp(N-1, K, W, V));
 
     }
 
-    public void dfs(int start, int sum, int size) {
-        for(int a = start; a < size; a++) {
+    public static int dp(int obj, int weight, int[] W, int[] V) {
+        if(obj < 0) {
+            return 0;
+        }
+
+        if(dp[obj][weight] == null) {
+            if(W[obj] > weight) dp[obj][weight] = dp(obj-1, weight, W, V);
+            else {
+                dp[obj][weight] = Math.max(dp(obj-1, weight, W, V), dp(obj-1, weight - W[obj], W, V) + V[obj]);
+            }
 
         }
+
+        return dp[obj][weight];
     }
 }
