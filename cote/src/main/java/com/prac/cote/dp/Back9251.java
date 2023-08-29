eff707c004dcd0169e5bf0b4d30dbbab716fe26d
WontaeSeong <won4932@gmail.com>
2023-08-29 10:41:48 +0900

    back 9251 Top-down c

--- cote/src/main/java/com/prac/cote/dp/Back9251.java
+++ cote/src/main/java/com/prac/cote/dp/Back9251.java
@@ -10,35 +10,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Back9251 {
 
+	static Integer[][] dp;
+
 	public static void main(String[] args) throws IOException {
 
 		SpringApplication.run(Back9251.class, args);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String lcs1 = br.readLine();
-		String lcs2 = br.readLine();
+		// String lcs1 = br.readLine();
+		// String lcs2 = br.readLine();
 
-		int[][] dp = new int[lcs1.length()][lcs2.length()];
+		char[] lcs1 = br.readLine().toCharArray();
+		char[] lcs2 = br.readLine().toCharArray();
 
-		int result = 0;
 
-		for(int i = lcs1.length()-1; i > 0; i--) {
-			result = Math.max(result, dfs(i, lcs2.length()-1, lcs1, lcs2, lcs2.length()-1, new int[lcs1.length()][lcs2.length()]));
-		}
-		System.out.println(result);
-		// System.out.println(dfs(lcs1.length()-1, lcs2.length()-1, lcs1, lcs2, lcs2.length()-1));
+		dp = new Integer[lcs1.length][lcs2.length];
+
+		System.out.println(dfs(lcs1.length-1, lcs2.length-1, lcs1, lcs2));
 	}
 
-	public static int dfs(int fi, int sec, String lcs1, String lcs2, int secEnd, int[][] dp) {
-		System.out.println(fi + " : " + sec + " , " + secEnd);
-		if(fi < 0) return 0;
+	public static int dfs(int fi, int sec, char[] lcs1, char[] lcs2) {
+		if(fi < 0 || sec < 0) return 0;
 
-		if(sec < 0) return dfs(fi-1, secEnd, lcs1, lcs2, secEnd, dp);
-
-		if(dp[fi][sec] == 0) {
-			if(lcs1.charAt(fi) != lcs2.charAt(sec)) dp[fi][sec] = dfs(fi, sec-1, lcs1, lcs2, secEnd, dp);
+		if(dp[fi][sec]==null) {
+			dp[fi][sec]=0;
+			if(lcs1[fi] == lcs2[sec]) {
+				dp[fi][sec] = dfs(fi-1, sec-1, lcs1, lcs2) + 1;
+			}
 			else {
-				dp[fi][sec] = Math.max(dfs(fi, sec-1, lcs1, lcs2, sec-1, dp), dfs(fi-1, sec-1, lcs1, lcs2, sec-1, dp)) + 1;
+				dp[fi][sec] = Math.max(dfs(fi-1, sec, lcs1, lcs2), dfs(fi, sec-1, lcs1, lcs2));
 			}
 		}
 
