8cd4b6a225faeb000b89aabae447286e9e335390
WontaeSeong <won4932@gmail.com>
2023-10-11 23:55:20 +0900

    back 2583 clear

--- cote/src/main/java/com/prac/cote/dp/Back2583.java
+++ cote/src/main/java/com/prac/cote/dp/Back2583.java
@@ -6,15 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 @SpringBootApplication
 public class Back2583 {
 
-    static boolean[] visited;
+    static int[] dx = {-1, 0, 1, 0};
 
-    static int[] result;
+    static int[] dy = {0, -1, 0, 1};
+
+//    static boolean[][] visited;
+
+    static int answer;
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(Back2583.class, args);
@@ -27,7 +33,10 @@ public class Back2583 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[][] graphPaper = new int[N+1][M+1];
+        int[][] graphPaper = new int[N][M];
+//        visited = new boolean[N][M];
+
+        List<Integer> result = new ArrayList<>();
 
         for(int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
@@ -38,30 +47,43 @@ public class Back2583 {
             int rx = Integer.parseInt(st.nextToken());
             int ry = Integer.parseInt(st.nextToken());
 
-            for(int a = lx; a <= rx; a++) {
-                for(int b = ly; b <= ry; b++) {
+            for(int a = lx; a < rx; a++) {
+                for(int b = ly; b < ry; b++) {
                     graphPaper[a][b] = 1;
                 }
             }
         }
 
-       for(int i = 0; i <= N; i++) {
-           for(int j = 0; j <= M; j++) {
-               dfs(i, j, graphPaper)
+       for(int i = 0; i < N; i++) {
+           for(int j = 0; j < M; j++) {
+               if(graphPaper[i][j] == 0) {
+                   answer = 0;
+                   dfs(i, j, graphPaper, M, N);
+                   result.add(answer);
+               }
+
            }
        }
+        System.out.println(result.size());
+        Collections.sort(result);
+        for (Integer i : result) {
+            System.out.print(i + " ");
 
+        }
     }
 
-    public static void dfs(int i, List<List<Integer>> list) {
-        visited[i] = true;
+    public static void dfs(int x, int y, int[][] graphPaper, int M, int N) {
+        answer++;
+        graphPaper[x][y] = 1;
 
-        for (Integer integer : list.get(i)) {
-            if(!visited[integer]) {
-                result[integer] = i;
-                bfs(integer, list);
+        for(int d = 0; d < 4; d++) {
+            int a = x + dx[d];
+            int b = y + dy[d];
+            if(a > -1 && b > -1 && a < N && b < M) {
+                if(graphPaper[a][b] == 0) {
+                    dfs(a, b,graphPaper, M, N);
+                }
             }
-
         }
     }
 }
