cc371248d715b130f3bc9b8bbee0e5db7e6b2311
WontaeSeong <won4932@gmail.com>
2023-11-22 23:20:34 +0900

    fixes

--- cote/src/main/java/com/prac/cote/CoteApplication.java
+++ cote/src/main/java/com/prac/cote/CoteApplication.java
@@ -9,13 +9,37 @@ import lombok.Getter;
 import java.io.IOException;
 import java.util.Scanner;
 
-//@SpringBootApplication
-//public class CoteApplication {
-//
-//	public static void main(String[] args) throws Exception {
-//		SpringApplication.run(CoteApplication.class, args);
-//
-//		Back1005 back1005 = new Back1005();
-//	}
-//
-//}
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        int[] A = new int[N + 1];
+
+        // 2부터 N까지의 index를 가지는 배열의 각 index에 index값을 삽입
+        for (int num = 2; num <= N; num++) {
+            A[num] = num;
+        }
+
+        // 2부터 N의 제곱근까지 반복
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            // 요소값이 0이면 지워진 것으로 판별
+            if (A[i] == 0) {
+                continue;
+            }
+
+            // i의 배수를 배열에서 모두 지움
+            for (int j = i + i; j <= N; j = j + i) {
+                A[j] = 0;
+            }
+        }
+
+        // 최소 범위부터 시작하여, 요소가 0이 아니면 소수로 판별
+        for (int i = M; i <= N; i++) {
+            if (A[i] != 0) {
+                System.out.println(A[i]);
+            }
+        }
+    }
+}
