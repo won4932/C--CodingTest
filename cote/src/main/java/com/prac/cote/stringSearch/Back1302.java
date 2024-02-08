08fc528016907b591de6ae98609b5923b5106d47
WontaeSeong <won4932@gmail.com>
2024-02-08 09:57:07 +0900

    Back 1302 c

--- cote/src/main/java/com/prac/cote/stringSearch/Back1302.java
+++ cote/src/main/java/com/prac/cote/stringSearch/Back1302.java
@@ -1,14 +1,10 @@
 package com.prac.cote.stringSearch;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.*;
 
-@SpringBootApplication
 public class Back1302 {
     static int N;
     public static void main(String[] args) throws IOException {
@@ -17,21 +13,24 @@ public class Back1302 {
 
         N = Integer.parseInt(br.readLine());
 
-        var set = new HashSet<String>();
+        var books = new HashMap<String, Integer>();
 
         for(int i = 0; i < N; i++) {
-            String word = br.readLine();
-
-            set.add(word);
+            String book = br.readLine();
+            books.put(book, books.getOrDefault(book, 0) +1);
         }
 
-        set.stream().sorted((o1, o2) -> {
-            if(o1.length() != o2.length()) {
-                return o1.length() - o2.length();
+        var list = new LinkedList<>(books.entrySet());
+
+        list.sort((o1, o2) -> {
+            if(Objects.equals(o1.getValue(), o2.getValue())) {
+                return o1.getKey().compareTo(o2.getKey());
             }else {
-                return o1.compareTo(o2);
+                return o2.getValue() - o1.getValue();
             }
-        }).forEach(System.out::println);
+        });
+
+        System.out.println(list.get(0).getKey());
 
     }
 }
