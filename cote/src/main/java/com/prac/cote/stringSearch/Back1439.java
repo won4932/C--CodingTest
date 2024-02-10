14ff811d8f28e1e7ddd51490d5ec16d626bc990a
WontaeSeong <won4932@gmail.com>
2024-02-10 09:57:07 +0900

    back 1439 c

--- cote/src/main/java/com/prac/cote/stringSearch/Back1439.java
+++ cote/src/main/java/com/prac/cote/stringSearch/Back1439.java
@@ -3,47 +3,40 @@ package com.prac.cote.stringSearch;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Back1439 {
-    static String[] grades = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
-    static Double[] subjectRating = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0};
-
-    static Integer[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        var majorRating = new HashMap<String, Double>();
+        int[] aa = new int[2];
 
-        for(int i = 0; i < grades.length; i++) {
-            majorRating.put(grades[i], subjectRating[i]);
+         String dasom = br.readLine();
+
+         int[] reserved = dasom
+             .chars()
+             .map(c -> c - '0')
+             .toArray();
+        int check = -1;
+
+        for(int i = 0; i < reserved.length; i++) {
+            if(check != reserved[i]) {
+                check = reserved[i];
+                aa[reserved[i]]++;
+            }
         }
 
-        double sumUnit = 0.0;
-        double sum = 0.0;
+        System.out.println(Math.min(aa[0], aa[1]));
 
-        for(int i = 0; i < 20; i++) {
-            st = new StringTokenizer(br.readLine());
+        //     if(check != reserved[i]) {
+        //         count++;
+        //         int swap = check;
+        //         check = reserved[i];
+        //         reserved[i] = swap;
+        //     }
+        //
+        // System.out.println(count);
 
-            String subjectName = st.nextToken();
-
-            double unit = Double.parseDouble(st.nextToken());
-
-            String grade = st.nextToken();
-
-            if(grade.equals("P")) continue;
-
-            sumUnit+= unit;
-
-            sum += (unit * majorRating.get(grade));
-        }
-
-        System.out.println(String.format("%.6f", sum / sumUnit));
     }
 
 }
