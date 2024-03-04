d6324eca21445b968c182f71ad6365d20bb75c0f
WontaeSeong <won4932@gmail.com>
2024-03-04 22:39:12 +0900

    Back 1941 c

--- cote/src/main/java/com/prac/cote/corporation/Back1941.java
+++ cote/src/main/java/com/prac/cote/corporation/Back1941.java
@@ -2,6 +2,7 @@ package com.prac.cote.corporation;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,13 +16,16 @@ public class Back1941 {
     static int result = 0;
     static boolean[][] visited = new boolean[5][5];
 
-    static int[][] student;
+    static char[][] student;
+
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        student = new int[5][5];
+        student = new char[5][5];
 
 
         for(int i = 0 ; i < 5; i++) {
@@ -50,8 +54,49 @@ public class Back1941 {
     }
 
     private static boolean checkPrincess() {
-        Queue<Integer> queue = new LinkedList<>();
+        boolean[][] cpyVisited = new boolean[5][5];
+        for (int i = 0; i < 5; i++) {
+            cpyVisited[i] = visited[i].clone();
+        }
 
+        int x = 0, y = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (cpyVisited[i][j]) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
 
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        Point p = new Point(x, y);
+
+        int cnt = 0;
+        int ids = 0;
+        while(!queue.isEmpty()) {
+            int[] coordinate = queue.poll();
+
+            for(int i = 0; i < 4; i++) {
+                int nx = coordinate[0] + dx[i];
+                int ny = coordinate[1] + dy[i];
+
+                if(0 > nx || 5 <= nx || 0 > ny || 5 <= ny) continue;
+
+                if(cpyVisited[nx][ny]) {
+                    if(student[nx][ny] == 'S') ids++;
+                    cnt++;
+                    queue.add(new int[]{nx, ny});
+                    cpyVisited[nx][ny] = false;
+                }
+            }
+        }
+
+        if(cnt == 7 && 4 <= ids) {
+            return true;
+        }
+
+        return false;
     }
 }
