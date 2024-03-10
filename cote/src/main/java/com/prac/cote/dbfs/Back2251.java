e2283696cc8abb7c3d7fd8b26fd5d27ef82d69ed
WontaeSeong <won4932@gmail.com>
2024-03-10 20:39:39 +0900

    Back 2251 c

--- cote/src/main/java/com/prac/cote/dbfs/Back2251.java
+++ cote/src/main/java/com/prac/cote/dbfs/Back2251.java
@@ -12,7 +12,7 @@ public class Back2251 {
     static boolean[][] vistied;
     static boolean[] answer;
 
-    static int A, B, C;
+    static int[] water;
 
     static int[] sender = {0 ,0 , 1, 1, 2, 2};
     static int[] receiver = {1, 2, 0, 2, 0, 1};
@@ -21,27 +21,33 @@ public class Back2251 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        water = new int[3];
+
         st = new StringTokenizer(br.readLine());
 
-        A = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        water[0] = Integer.parseInt(st.nextToken());
+        water[1] = Integer.parseInt(st.nextToken());
+        water[2] = Integer.parseInt(st.nextToken());
 
         vistied = new boolean[201][201];
         answer = new boolean[201];
 
         bfs();
+
+        for(int i=0; i<answer.length; i++) {
+            if(answer[i]) System.out.print(i+ " ");
+        }
     }
 
     public static void bfs() {
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{0, 0});
         vistied[0][0] = true;
-        answer[C] = true;
+        answer[water[2]] = true;
 
         while(!queue.isEmpty()) {
             int[] ab = queue.poll();
-            int c = C - ab[0] - ab[1];
+            int c = water[2] - ab[0] - ab[1];
 
             for(int i = 0; i < 6; i++) {
                 int[] next = {ab[0], ab[1], c};
@@ -49,7 +55,17 @@ public class Back2251 {
                 next[receiver[i]] += next[sender[i]];
                 next[sender[i]] = 0;
 
-                if(next[receiver[i]] > )
+                if(next[receiver[i]] > water[receiver[i]]) {
+                    next[sender[i]] = next[receiver[i]] - water[receiver[i]];
+                    next[receiver[i]] = water[receiver[i]];
+
+                }
+                if(!vistied[next[0]][next[1]]) {
+                    vistied[next[0]][next[1]] = true;
+                    queue.add(new int[]{next[0], next[1]});
+                    if(next[0] == 0) answer[next[2]] = true;
+                }
+
 
             }
         }
