cd5bae0ecaa5b78fc56dbaf0b8224b1bff08c2b0
WontaeSeong <won4932@gmail.com>
2024-04-09 14:38:22 +0900

    back 9012 c

---
+++ cote/src/main/java/com/prac/cote/retry/Back9012ST.java
@@ -0,0 +1,42 @@
+package com.prac.cote.retry;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+@SpringBootApplication
+public class Back9012ST {
+
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            String line = br.readLine();
+
+            Stack<Character> sk = new Stack<>();
+
+            for(int j = 0; j < line.length(); j++) {
+                char c = line.charAt(j);
+
+                if(sk.isEmpty()) sk.push(c);
+                else {
+                    if(c == ')' && sk.peek() == '(') sk.pop();
+                    else sk.push(c);
+                }
+            }
+
+            if(sk.empty())
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
