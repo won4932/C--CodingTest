49ec52f90012c5335cf57fa6da75402d16727aeb
WontaeSeong <won4932@gmail.com>
2024-04-10 14:43:44 +0900

    Back 1316 c

---
+++ cote/src/main/java/com/prac/cote/stringSearch/Back1316.java
@@ -0,0 +1,48 @@
+package com.prac.cote.stringSearch;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+@SpringBootApplication
+public class Back1316 {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        for(int i = 0; i < N; i++) {
+            String word = br.readLine();
+
+            var set = new HashSet<Character>();
+
+            char sequenceCheck = 0;
+
+            boolean check = true;
+
+            for(int j = word.length()-1; j >= 0; j--) {
+                char now = word.charAt(j);
+
+                if(set.contains(now) && sequenceCheck != now) {
+                    check = false;
+                    continue;
+                }else {
+                    set.add(now);
+                }
+
+                sequenceCheck = now;
+            }
+
+            if(check) count++;
+        }
+
+        System.out.println(count);
+    }
+}
