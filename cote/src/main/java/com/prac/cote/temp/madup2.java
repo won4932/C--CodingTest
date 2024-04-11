dfa0096cedd01df66af056cf9fe0ac43f3b31994
WontaeSeong <won4932@gmail.com>
2024-04-11 11:04:12 +0900

    madup2 Q test

--- cote/src/main/java/com/prac/cote/temp/madup2.java
+++ cote/src/main/java/com/prac/cote/temp/madup2.java
@@ -16,10 +16,6 @@ public class madup2 {
                             {0, 2, 0, 1, 1},
                             {0, 0, 2, 0, 0},
                             {0, 0, 1, 0, 0}};
-
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
-
     static int rech = 2;
 
     static boolean[][] visited = new boolean[5][5];
@@ -40,8 +36,10 @@ public class madup2 {
             for(int j = 0; j < area[i].length; j++) {
                 if(area[i][j] == 1 && !visited[i][j]) {
                     visited[i][j] = true;
-                    dfs(i, j, 0, true);
-                    dfs(i, j, 0, false);
+                    dfs(i-1, j, 0, true);
+                    dfs(i+1, j, 0, true);
+                    dfs(j-1, i, 0, false);
+                    dfs(j+1, i, 0, false);
                 }
             }
         }
@@ -59,41 +57,22 @@ public class madup2 {
     static private void dfs(int a, int fix, int count, boolean xy) {
         if(count == rech) return;
 
-        int ba = a - 1;
-        int na = a + 1;
-
-        if(ba < 5 && ba > 0 && fix < 5 && fix > 0) {
+        if(a < 5 && a > -1) {
             if(xy) {
-                if(area[ba][fix] == 0) {
-                    area[ba][fix] = 1;
-                    dfs(ba, fix, count+1, true);
+                if(area[a][fix] == 0) {
+                    visited[a][fix] = true;
+                    area[a][fix] = 1;
+                    dfs(a, fix, count+1, true);
                 }
             }else {
-                if(area[fix][ba] == 0) {
-                    area[fix][ba] = 1;
-                    dfs(ba, fix, count+1, false);
+                if(area[fix][a] == 0) {
+                    visited[fix][a] = true;
+                    area[fix][a] = 1;
+                    dfs(a, fix, count+1, false);
                 }
             }
 
 
         }
-
-        if(na < 5 && na > 0 && fix < 5 && fix > 0) {
-            if(xy) {
-                if(area[na][fix] == 0) {
-                    area[na][fix] = 1;
-                    dfs(na, fix, count+1, true);
-                }
-            }else {
-                if(area[fix][na] == 0) {
-                    area[fix][na] = 1;
-                    dfs(na, fix, count+1, false);
-                }
-            }
-
-
-        }
-
-
     }
 }
