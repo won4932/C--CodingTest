85ee977470710845e4698f7bc7196013c2949a37
WontaeSeong <won4932@gmail.com>
2024-04-21 16:54:34 +0900

    Back 11478 c

---
+++ cote/src/main/java/com/prac/cote/corporation/Back15439.java
@@ -0,0 +1,25 @@
+package com.prac.cote.corporation;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+@SpringBootApplication
+public class Back15439 {
+
+    static int N;
+    static int[] aggregate;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        System.out.println(N * (N-1));
+
+    }
+}
