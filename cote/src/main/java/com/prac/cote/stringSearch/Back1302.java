ce8cb15d9a9b2cbb1b301631c32ec2917cff7a23
WontaeSeong <won4932@gmail.com>
2024-04-21 16:04:23 +0900

    Back 1302 c

---
+++ cote/src/main/java/com/prac/cote/stringSearch/Back1302.java
@@ -0,0 +1,37 @@
+package com.prac.cote.stringSearch;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+@SpringBootApplication
+public class Back1302 {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        var set = new HashSet<String>();
+
+        for(int i = 0; i < N; i++) {
+            String word = br.readLine();
+
+            set.add(word);
+        }
+
+        set.stream().sorted((o1, o2) -> {
+            if(o1.length() != o2.length()) {
+                return o1.length() - o2.length();
+            }else {
+                return o1.compareTo(o2);
+            }
+        }).forEach(System.out::println);
+
+    }
+}
