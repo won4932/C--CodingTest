dfaf5927ceadd96964515961a095e4f24ad90aa8
WontaeSeong <won4932@gmail.com>
2024-05-17 15:22:34 +0900

    test

--- cote/src/main/java/com/prac/cote/etc/dkteckin2404.java
+++ cote/src/main/java/com/prac/cote/etc/dkteckin2404.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,17 +22,26 @@ public class dkteckin2404 {
 
         String str = "[200][http://naver.com][FX][2012-06-10 08:00:00]";
 
-        Matcher matcher = pattern.matcher(str);
+        // Matcher matcher = pattern.matcher(str);
+        //
+        // while(matcher.find()) {
+        //     System.out.println(matcher.group());
+        // }
+        //
+        // if(matcher.find()) {
+        //     System.out.println(matcher.group(1));
+        //     System.out.println(matcher.group(2));
+        //     System.out.println(matcher.group(3));
+        // }
 
-        while(matcher.find()) {
-            System.out.println(matcher.group());
-        }
+        // System.out.println(Integer.parseInt("000000000000000000000000000000100"));
 
-        if(matcher.find()) {
-            System.out.println(matcher.group(1));
-            System.out.println(matcher.group(2));
-            System.out.println(matcher.group(3));
-        }
+        BigInteger big = new BigInteger("11");
+        BigInteger big2 = new BigInteger("11");
+
+        big = big.add(big2);
+
+        System.out.println(big.toString());
     }
 
 }
