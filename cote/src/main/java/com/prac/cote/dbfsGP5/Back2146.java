7a5fc8352f913608a7fc9f72760f4525b1c7e458
WontaeSeong <won4932@gmail.com>
2024-05-23 16:55:14 +0900

    back 2146 c

--- cote/src/main/java/com/prac/cote/dbfsGP5/Back2146.java
+++ cote/src/main/java/com/prac/cote/dbfsGP5/Back2146.java
@@ -15,9 +15,12 @@ public class Back2146 {
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
 
-    static boolean[][] visitied;
+    static boolean[][] visited;
 
     static int min = Integer.MAX_VALUE;
+
+    // static Queue<Node> queue = new LinkedList<>();
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,6 +30,7 @@ public class Back2146 {
         N = Integer.parseInt(br.readLine());
 
         islands = new int[N][N];
+        visited = new boolean[N][N];
 
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -36,43 +40,103 @@ public class Back2146 {
             }
         }
 
+        int color = 1;
 
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(islands[i][j] == 1 && !visited[i][j]) {
+                    land(i, j, color);
+                    color++;
+                }
+
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(islands[i][j] != 0) {
+                    visited = new boolean[N][N];
+                    search(i, j, islands[i][j]);
+                }
+
+            }
+        }
+
+        System.out.println(min-1);
     }
 
-    private static boolean bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0});
-        visitied[0][0] = true;
+    private static void land(int y, int x, int color) {
+        Queue<int[]> landQueue = new LinkedList<>();
+        landQueue.add(new int[]{y, x});
+        visited[y][x] = true;
+        islands[y][x] = color;
 
-        boolean ck = false;
+        while(!landQueue.isEmpty()) {
+            int[] xy = landQueue.poll();
 
-        int chees = 0;
+            for(int i = 0; i < 4; i++) {
+                int nx = xy[1] + dx[i];
+                int ny = xy[0] + dy[i];
+
+                if(nx < N && nx > -1 && ny < N && ny > -1 && !visited[ny][nx] && islands[ny][nx] > 0) {
+                    visited[ny][nx] = true;
+                    islands[ny][nx] = color;
+                    landQueue.add(new int[]{ny, nx});
+                }
+            }
+        }
+    }
+
+    private static void search(int y, int x, int color) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(x, y, color, 0));
+
+        visited[y][x] = true;
 
         while(!queue.isEmpty()) {
-            int xy[] = queue.poll();
+            Node node = queue.poll();
 
-            int nowX = xy[1];
-            int nowY = xy[0];
+            if(node.count+1 > min) break;
+
+            int nowX = node.x;
+            int nowY = node.y;
 
             for(int i = 0; i < 4; i++) {
                 int nx = nowX + dx[i];
                 int ny = nowY + dy[i];
 
-                if(nx < X && nx > -1 && ny < Y && ny > -1 && !visitied[ny][nx]) {
-                    visitied[ny][nx] = true;
-                    if(board[ny][nx] == 1) {
-                        chees++;
-                        ck = true;
-                        boardClone[ny][nx] = 0;
-                    }else {
-                        queue.add(new int[]{ny, nx});
+                if(nx < N && nx > -1 && ny < N && ny > - 1 && !visited[ny][nx]) {
+                    if(islands[ny][nx] == 0) {
+                        queue.add(new Node(nx, ny, node.color, node.count+1));
+                        visited[ny][nx] = true;
                     }
+                    else if(islands[ny][nx] != node.color) {
+                        min = Math.min(min, node.count+1);
+                    }else {
+                        visited[ny][nx] = true;
+                    }
+
                 }
             }
         }
+    }
 
-        cheese = chees == 0 ? cheese : Math.min(cheese, chees);
+    private static void bfs() {
 
-        return ck;
+    }
+
+    public static class Node {
+        int x;
+        int y;
+        int color;
+
+        int count;
+
+        public Node(int x, int y, int color, int count) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            this.count = count;
+        }
     }
 }
