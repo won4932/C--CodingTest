69799237908ccb5c10c376ee5cda6f7293652b35
WontaeSeong <won4932@gmail.com>
2024-05-27 09:35:08 +0900

    back 5525 c

--- cote/src/main/java/com/prac/cote/stringSearch/Back5525.java
+++ cote/src/main/java/com/prac/cote/stringSearch/Back5525.java
@@ -3,13 +3,8 @@ package com.prac.cote.stringSearch;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.StringTokenizer;
-import java.util.regex.Pattern;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Back5525 {
     static int N, M;
 
@@ -26,19 +21,41 @@ public class Back5525 {
         M = Integer.parseInt(br.readLine());
 
         S = br.readLine();
-
-        for(int i = 1; i < N; i++) {
-            SN.append("OI");
-        }
-
-        int start = 0;
+        //
+        // for(int i = 1; i < N; i++) {
+        //     SN.append("OI");
+        // }
 
         int count = 0;
 
-        while((start = S.indexOf(SN.toString(), start)) != -1) {
-            count++;
-            start++;
+        int pnCount = 0;
+
+        for(int i = 1; i < M-1; i++) {
+            if(S.charAt(i) == 'O' && S.charAt(i + 1) == 'I') {
+                pnCount++;
+                if(pnCount == N) {
+                    if(S.charAt(i-(pnCount*2-1))=='I') count++;
+                pnCount--;
+                }
+                i++;
+            }else {
+                pnCount = 0;
+            }
         }
+
+        // for(int i = 0; i < M - SN.length() + 1; i++) {
+        //     if(SN.charAt(0) == S.charAt(i)) {
+        //         if(SN.toString().equals(S.substring(i, SN.length() + i))) {
+        //             count++;
+        //             i++;
+        //         }
+        //     }
+        // }
+
+        // while((start = S.indexOf(SN.toString(), start)) != -1) {
+        //     count++;
+        //     start++;
+        // }
 
         System.out.println(count);
     }
